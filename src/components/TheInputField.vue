704b19a172352065f6eb0ba354970fa74d69803f
Marinario <ilbuonomosharro@gmail.com>
2023-07-23 11:20:31 +0200

    src/components : Created new file TheInputField.vue wich is a reusable component for the input fields

---
+++ src/components/TheInputField.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  pType: {
+    required: true,
+    type: String,
+  },
+  pPlaceholder: {
+    required: true,
+    type: String,
+  },
+  pRequired: {
+    type: Boolean,
+  },
+  pClass: {
+    required: true,
+    type: String,
+  },
+  pLabel: {
+    required: true,
+    type: String,
+  },
+  modelValue: {
+    required: true,
+    type: String,
+  },
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="form-floating mb-4">
+    <input :type="props.pType" :class="props.pClass" :value="props.modelValue"
+           @input="$emit('update:modelValue', $event.target.value)"
+           :placeholder="props.pPlaceholder" :required="props.pRequired">
+    <label for="floatingInput">{{ pLabel }}</label>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
