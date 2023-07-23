06c7fd521127d6a622479975553af2626dd685f3
Marinario <ilbuonomosharro@gmail.com>
2023-07-23 10:54:04 +0200

    src/components : Created new file named BaseLayout, deleted HelloWorld. src/App.vue : removed HelloWorld. Src : Deleted index.css. src/main.js : imported bootstrap css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 import {createPinia} from "pinia";
 import router from "./routers/index.js";
