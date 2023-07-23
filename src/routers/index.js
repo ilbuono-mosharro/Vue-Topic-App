c353756233100e54275f3ffdff5290d6f684385c
Marinario <ilbuonomosharro@gmail.com>
2023-07-23 10:46:30 +0200

    src/routes/index : Created index.js file which maintains all the configurations for the routers

---
+++ src/routers/index.js
@@ -0,0 +1,12 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
+
+export default router
