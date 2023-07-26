114390f96a24fab235ec0059ba4de5f381a935ae
Marinario <ilbuonomosharro@gmail.com>
2023-07-26 19:28:51 +0200

    src/stores/topics/UserTopicsStore.js Created store for manage user topics, added link in the nav

---
+++ src/stores/topics/UserTopicsStore.js
@@ -0,0 +1,33 @@
+import {defineStore} from 'pinia'
+import axios from "axios";
+
+export const useUserTopicsStore = defineStore('userTopics', {
+    state: () => (
+        {
+            data: null,
+            loading: false,
+            error: null,
+        }
+    ),
+    actions: {
+        async userTopics(token) {
+            this.loading = true
+            try {
+                const response = await axios.get(`http://127.0.0.1:8000/api/topics/user-topics/`, {},
+                    {
+                        headers: {
+                            "Authorization": token,
+                        },
+                    })
+                this.data = response.data
+            } catch (e) {
+                this.error = e.response.data
+            } finally {
+                this.loading = false;
+            }
+        },
+    },
+})
+
+
+
