ef1f28e3302da39375f1ee06b46c7ea84128b501
Marinario <ilbuonomosharro@gmail.com>
2023-07-26 12:11:14 +0200

    Refactored

---
+++ src/views/messages/TheDelete.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheMessage from "../../components/TheMessage.vue";
+</script>
+
+<template>
+  <TheMessage p-class="alert alert-success" p-htext="Well done!" p-text="Your account has been deleted successfully."
+              p-to="/" p-link-text="Go to the home page."/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/messages/TheInfo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheMessage from "../../components/TheMessage.vue";
+</script>
+
+<template>
+  <TheMessage p-class="alert alert-success" p-htext="Well done!" p-text="You have registered successfully."
+                p-to="/login" p-link-text="Go to the login page."/>
+</template>
+
+<style scoped>
+
+</style>
